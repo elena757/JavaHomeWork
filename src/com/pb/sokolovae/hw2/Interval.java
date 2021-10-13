27917c3d0f4a5201b9d2aa56c5a510aa55c31755
Sokolova Elena <dp040880ses1@gmail.com>
2021-10-14 00:25:31 +0300

    init commit

---
+++ src/com/pb/sokolovae/hw2/Interval.java
@@ -0,0 +1,22 @@
+package com.pb.sokolovae.hw2;
+
+import java.util.Scanner;
+
+public class Interval {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите любое целое число");
+        int Numeric;
+        Numeric = scanner.nextInt();
+        if (Numeric >= 0 && Numeric <= 14) {
+            System.out.println("Супер! Вы попали в интервал [0 -14]");
+        } else if (Numeric >= 15 && Numeric <= 35) {
+            System.out.println("Вау! Вы попали в интервал [15 - 35]");
+        } else if (Numeric >= 36 && Numeric <= 50) {
+            System.out.println("Огонь! Вы попали в интервал [36 - 50]");
+        } else if (Numeric >= 51 && Numeric <= 100) {
+            System.out.println("Превосходно! Вы попали в интервал [51 - 100]");
+        } else
+            System.out.println("Эх нет такого интервала в нашей програмке");
+    }
+}
